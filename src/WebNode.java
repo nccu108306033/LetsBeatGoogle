60399289bfd5bfd79d8b50b686391c27a762c7d2
FrankHsu <105202040@mail2.nccu.tw>
2020-12-28 14:47:31 +0800

    update

--- src/WebNode.java
+++ src/WebNode.java
@@ -1,4 +1,26 @@
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class WebNode {
-
+	public WebNode parent;
+	public ArrayList<WebNode> children;
+	public WebPage webPage;
+	public double nodeScore;
+	
+	public WebNode(WebPage webPage){
+		this.webPage = webPage;
+		this.children = new ArrayList<WebNode>();
+	}
+	
+	public void setNodeScore(ArrayList<Keyword> keywords) throws IOException{
+		webPage.setScore(keywords);
+		nodeScore = webPage.score;
+		for(WebNode child : children){
+			nodeScore += child.nodeScore;
+		}
+	}
+	public void addChild(WebNode child){
+		this.children.add(child);
+		child.parent = this;
+	}
 }
---
+++ src/WebTree.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+public class WebTree {
+	public WebNode root;
+	
+	public WebTree(WebPage rootPage){
+		this.root = new WebNode(rootPage);
+	}
+	
+	public void setPostOrderScore(ArrayList<Keyword> keywords) throws IOException{
+		setPostOrderScore(root, keywords);
+	}
+	
+	private void setPostOrderScore(WebNode startNode, ArrayList<Keyword> keywords) throws IOException{
+		for(WebNode child : startNode.children){
+			setPostOrderScore(child,keywords);
+			child.setNodeScore(keywords);
+		}
+		startNode.setNodeScore(keywords);
+	}
+}
