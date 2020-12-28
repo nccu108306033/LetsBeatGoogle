443ee886771eaa69ec257943985d88599c3cb45f
FrankHsu <105202040@mail2.nccu.tw>
2020-12-28 14:05:59 +0800

    update

--- src/Keyword.java
+++ src/Keyword.java
@@ -1,19 +1,14 @@
 
 public class Keyword {
 	public String name;
-	public int count;
 	public float weight;
 	
-	public Keyword(String name,int count,float weight) {
+	public Keyword(String name,float weight) {
 		this.name = name;
-		this.count = count;
 		this.weight = weight;
 	}
 	public String getName() {
 		return name;
-	}
-	public int getCount() {
-		return count;
 	}
 	public float getWeight() {
 		return weight;
@@ -21,7 +16,7 @@ public class Keyword {
 	
 	@Override
 	public String toString() {
-		return "["+name+","+count+","+weight+"]";
+		return "["+name+","+weight+"]";
 	}
 	
 }
---
+++ src/WebPage.java
@@ -0,0 +1,7 @@
+
+public class WebPage {
+	public String url;
+	public String name;
+	public double score;
+	public WordCounter counter;
+}
